0d9153ba8513e9779ae732bcac3171857b289cd9
bigmsg <travelcar@naver.com>
2019-10-26 18:17:41 +0900

    차트 구현, multiplier사용 multiplier는 값 변경이 안됨, 오직 읽기만 가능하기 때문에 새로 만들어서 적용해야 함 그래서 extension 사용해서 새로만들어 적용

--- AutoLayout_Chart/ViewController.swift
+++ AutoLayout_Chart/ViewController.swift
@@ -8,13 +8,90 @@
 
 import UIKit
 
+extension NSLayoutConstraint {
+    func changeMultiplier (value: CGFloat) -> NSLayoutConstraint {
+        
+        // 기존 costraint 비활성화시킴
+        NSLayoutConstraint.deactivate([self])
+        
+         let newConstraint = NSLayoutConstraint.init(item: self.firstItem, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: value, constant: self.constant)
+        
+        // 기존 속성들은 그대로 복사
+        newConstraint.priority = self.priority
+        newConstraint.shouldBeArchived = self.shouldBeArchived
+        newConstraint.identifier = self.identifier
+        
+        // 지금 만든 것은 활성화
+        NSLayoutConstraint.activate([newConstraint])
+        
+        return newConstraint
+    }
+}
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var graph1Height: NSLayoutConstraint!
+    @IBOutlet weak var graph2Height: NSLayoutConstraint!
+    @IBOutlet weak var graph3Height: NSLayoutConstraint!
+    @IBOutlet weak var graph4Height: NSLayoutConstraint!
+    @IBOutlet weak var graph5Height: NSLayoutConstraint!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        //graph1Height.constant = 500
+        //graph1Height.multiplier = 0.5
+        
+
+        
+        
     }
 
+    
+    @IBAction func style1(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.7) {
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.9)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.7)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.5)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.3)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.1)
+            self.view.layoutIfNeeded()
+        }
+        /*UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.9)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.7)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.5)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.3)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.1)
+            
+            self.view.layoutIfNeeded()      // 화면갱신 함수
+            
+        }, completion: { (completion) in
+            // 애니메이션 끝나는 시점
+            self.dismiss(animated: false, completion: nil)
+        })*/
+        
 
+        
+    }
+    
+    @IBAction func style2(_ sender: Any) {
+        
+        graph1Height = graph1Height.changeMultiplier(value: 0.1)
+        graph2Height = graph2Height.changeMultiplier(value: 0.3)
+        graph3Height = graph3Height.changeMultiplier(value: 0.7)
+        graph4Height = graph4Height.changeMultiplier(value: 0.8)
+        graph5Height = graph5Height.changeMultiplier(value: 0.3)
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
 }
 
